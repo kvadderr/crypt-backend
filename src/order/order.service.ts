1243c678374696f5e9524bb0fcfa19e4b5affd1e
itigertechno <itigertechno@gmail.com>
2023-02-28 01:48:03 +0600

    pap

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Order } from './order.model';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
+import fetch from "node-fetch";
 
 @Injectable()
 export class OrderService {
@@ -20,6 +20,31 @@ export class OrderService {
             HttpStatus.BAD_REQUEST,
           );
         }
+
+        const HTML_MESSAGE = '\
+        <b>Новая заявка на вывод средств!<b>\
+        <br>\
+        <p>'+ orderData.amount + ' ' + orderData.crypt + '</p>\
+        <br>\
+        <p>Способ вывода: ' + orderData.payment + '</p>\
+        <p>Валюта для вывода: ' + orderData.currency + '</p>\
+        ';
+
+        data = {
+            "chat_id": -1001758934936,
+            "text": HTML_MESSAGE,
+            "parse_mode": "HTML"
+        }
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        };
+
+        await fetch(
+            'https://api.telegram.org/bot5828856128:AAGwW4JLpeX0B4rQ1uyuGbcGLvxnqk3SFZE/sendMessage', 
+            requestOptions)
     
         return orderData;
     }
