f374e8f23f6a17e5543587b7b8747369cdcba59d
itigertechno <itigertechno@gmail.com>
2023-03-01 14:51:06 +0600

    pap

--- src/order/order.model.ts
+++ src/order/order.model.ts
@@ -24,6 +24,9 @@ export class Order extends AppEntity {
 
     @Column({
 	nullable: true,
+        default: 0,
+        type: 'numeric', 
+        precision: 10, scale: 5 
     })
     amount: number;
 
